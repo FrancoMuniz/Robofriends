7db7aacb7f21632195bfe7b64fb8bff6c7ef7cc6
Franco <francorulos08@gmail.com>
2019-12-05 23:52:47 -0300

    Updates

---
+++ precache-manifest.88cc0813307e9b1e0cb88a3049f2cada.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7dba06595db87bc74f912d1f70b233b",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6cee7e850ba14c8e9c97",
+    "url": "/Robofriends/static/css/main.0c30f6ae.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/Robofriends/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "6cee7e850ba14c8e9c97",
+    "url": "/Robofriends/static/js/main.cf4ea288.chunk.js"
+  },
+  {
+    "revision": "a3eff8e1727534c00f37",
+    "url": "/Robofriends/static/js/runtime-main.11a3c7d6.js"
+  },
+  {
+    "revision": "7cc222a3be796abb86c6308b5862ff59",
+    "url": "/Robofriends/static/media/Urba.7cc222a3.woff"
+  }
+]);
